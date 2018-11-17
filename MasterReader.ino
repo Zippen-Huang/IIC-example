b285b05bfc4a925c1dedf5cb06ae9c7aa1739f5d
Zippen-Huang <1508303949@qq.com>
2018-11-17 10:42:08 +0800

    Add files via upload

---
+++ MasterReader.ino
@@ -0,0 +1,94 @@
+#include <Wire.h>
+
+bool tf =false;
+char iicData[20];//IIC传送的字符数组
+char Device_Address = 8;
+#define I2C_ERROR                  (-1)
+float recData[3]; 
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop() {  
+  char return_data = read_Data(8, iicData, 20);
+  if(return_data == 0){
+      char2floatFunc(iicData,20);
+      for(int j=0;j<3;j++){
+        Serial.print(recData[j]);
+        Serial.print(" ");
+      }
+  }
+  else{
+    Serial.print(return_data);
+  }
+  Serial.println("-----------------");
+  delay(100);
+}
+
+//字符数组转浮点数组
+void char2floatFunc(char* iicdata,char iic_size){
+  char intervalArr[]="";
+  int n=0;
+  int cn=0;
+  for(int i=0;i<iic_size;i++){
+    if(n>3)
+    {
+      n=0;
+      break;
+      }
+     
+    if(iicData[i] == 'X'){
+      recData[n]=atof(intervalArr);
+      //Serial.print(recData[n]);
+      n++;
+      cn=i;
+      //Serial.println(cn); 
+    }
+    else
+    {
+      if(n==0)
+      {
+        intervalArr[i]=iicData[i];
+        }
+      else
+      {
+        intervalArr[i-cn-1]=iicData[i];
+        //Serial.print(intervalArr[i-cn]);
+        }
+      }
+  }
+//  Serial.println(recData[0]); 
+}
+
+//读取IIC缓存区数据
+char read_Data(char start, char *buffer, char size)
+{
+  int16_t i = 0;
+  int8_t return_value = 0;
+  Wire.beginTransmission(Device_Address);
+  return_value = Wire.write(start);
+  if(return_value != 1)
+  {
+    return(I2C_ERROR);
+  }
+  return_value = Wire.endTransmission(false);
+  if(return_value != 0)
+  {
+    return(return_value);
+  }
+  delayMicroseconds(1);
+  /* Third parameter is true: relase I2C-bus after data is read. */
+  Wire.requestFrom(Device_Address, size, (uint8_t)true);
+  while(Wire.available() && i < size)
+  {
+    buffer[i++] = Wire.read();
+  }
+  delayMicroseconds(1);
+  if(i != size)
+  {
+    return(I2C_ERROR);
+  }
+  return(0); //return: no error 
+}
---
+++ SlaveSender.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+
+char *c;
+char cstr[20];//临时用于储存的字符数组
+float tf_data[3]={-8.20,-0.35,-3.67};//需要通过IIC传输的数组
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(200);
+//  c=arr_tf(tf_data,3);
+//   arr_tf(tf_data,3,cstr);
+//  Serial.println(strlen(c));
+//  Serial.println("****");
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  char* s = arr_tf(tf_data,3);
+  Wire.write(s,strlen(s)); 
+//  delay(2);
+//  delayMicroseconds(100);
+}
+
+char* arr_tf(float arr[],int n)//浮点数数组转字符数组
+{
+  int i;
+  //char *_cstr = new char[20];
+  cstr[20]="";
+  for(i=0;i<n;i++)
+  {
+    char intel_char[]="";
+    dtostrf(arr[i],2,2,intel_char);
+    if(i==0)
+    {
+      strcpy(cstr,intel_char);
+      }
+     else{
+      strcat(cstr,intel_char);
+     }
+     strcat(cstr,"X");
+ //    delayMicroseconds(5);
+  }
+  Serial.println(cstr);
+  return cstr;
+}
